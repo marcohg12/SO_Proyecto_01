6442093997433bd7746534ff4315a8007851a1e2
marcohg12 <marcohg0312@gmail.com>
2024-09-06 21:27:36 -0600

    compresion v2

--- main.c
+++ main.c
@@ -7,12 +7,14 @@
 #include "wchar.h"
 
 void serial_comp(char* dir_path);
+void serial_decomp(char* dir_path);
 
 int main(){
 
     setlocale(LC_ALL, "");
 
     serial_comp("/home/marco/Descargas/textos");
+    //serial_decomp("/home/marco/Descargas/comp_temp");
     
     return 0;
 }
--- serial_comp.h
+++ serial_comp.h
@@ -3,5 +3,7 @@
 
 void serial_comp(char* dir_path);
 void compress_file(char* file_path, char* filename, char* comp_dir_path);
+void serial_decomp(char* dir_path);
+void decompress_file(char* file_path, char* filename, char* decomp_dir_path);
 
 #endif
