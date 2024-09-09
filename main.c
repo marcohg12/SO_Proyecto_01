0819dbd4b097cec91917c2677188971f1b97eec3
Arianaam335 <arianaam335@estudiantec.cr>
2024-09-08 22:42:20 -0600

    fork

--- main.c
+++ main.c
@@ -7,13 +7,14 @@
 #include "wchar.h"
 
 //void parallel_comp(char* dir_path);
-void parallel_decomp(char* dir_path);
+//void parallel_decomp(char* dir_path);
+void serial_comp(char* dir_path);
 
 int main() {
 
     setlocale(LC_ALL, "");
 
-    parallel_decomp("/home/arianaalvarado/Descargas/Proyecto1_SO/SO_Proyecto_01/parallel_comp_temp");
+    serial_comp("/home/arianaalvarado/Descargas/Proyecto1_SO/SO_Proyecto_01/Libros");
     
     //serial_comp("/home/marco/Escritorio/Proyectos/SO_Proyecto_01/Libros");
     //serial_decomp("/home/marco/Escritorio/Proyectos/SO_Proyecto_01/Libros.tar");
